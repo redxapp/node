d1b7651e6cd417113e37c5b1256604513ecb8a34
redxapp <154985386+redxapp@users.noreply.github.com>
2023-12-27 05:06:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//mongoDB conection and routes
+
 const api=require('./routes/api');
 app.use('/api',api);
 
